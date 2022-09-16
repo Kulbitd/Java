d0b8eefb808392116b631acbb54a20bd8166c1f8
Данил <kulbit@MacBook-Pro-Danil.local>
2022-09-16 20:53:05 +0300

    Выполнил 1 задание

--- pz-kulbaka/src/kulbaka/Pz0.java
+++ pz-kulbaka/src/kulbaka/Pz0.java
@@ -3,18 +3,30 @@ package kulbaka;
 import java.util.Scanner;
 
 public class Pz0 {
-	public static void ex0(String[] args) {
-		try (Scanner in = new Scanner(System.in)) {
+	public static int ex0() {
+		try (Scanner in = new Scanner(System.in))
+		{
 			System.out.print("Input a equation [a/b]: ");
 			String letter = in.next();
-			if (letter == "b")
+			if (letter.contains("a"))
 			{
-				System.out.print("Input a x: ");
+				System.out.print("Input x: ");
 				double x = in.nextDouble();
 				double y = 17 * Math.pow(x,2) - 6 * x + 13 ;
 				System.out.print(y);
 			}
-				
+			if (letter.contains("b"))
+			{
+				System.out.print("Input a: ");
+				double a = in.nextDouble();
+				double y = 3 * Math.pow(a,2) + 5 * a - 21 ;
+				System.out.print(y);
+			}
+		return 0;
 		}
     }
+	public static void main(String[] args)
+	{
+		System.out.print(ex0());
+	}
 }
