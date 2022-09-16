d733854814439df81fdaaf9d1eefb2ee977ee06e
Данил <kulbit@MacBook-Pro-Danil.local>
2022-09-16 20:35:26 +0300

    Добавил выбор уравнения и решение первого уравнения

--- pz-kulbaka/src/kulbaka/Pz0.java
+++ pz-kulbaka/src/kulbaka/Pz0.java
@@ -1,5 +1,20 @@
 package kulbaka;
 
-public class Pz0 {
+import java.util.Scanner;
 
+public class Pz0 {
+	public static void ex0(String[] args) {
+		try (Scanner in = new Scanner(System.in)) {
+			System.out.print("Input a equation [a/b]: ");
+			String letter = in.next();
+			if (letter == "b")
+			{
+				System.out.print("Input a x: ");
+				double x = in.nextDouble();
+				double y = 17 * Math.pow(x,2) - 6 * x + 13 ;
+				System.out.print(y);
+			}
+				
+		}
+    }
 }
