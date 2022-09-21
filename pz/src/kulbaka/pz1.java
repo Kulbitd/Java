86aeac4d9324fe55a0c9d091c03c967395a8f42e
Данил <kulbit@MacBook-Pro-Danil.local>
2022-09-21 13:11:00 +0300

    Выполнил 1.2

--- pz/src/kulbaka/pz1.java
+++ pz/src/kulbaka/pz1.java
@@ -1,5 +1,10 @@
 package kulbaka;
 
 public class pz1 {
-
+	public static void ex2() {
+		System.out.println("         1");
+		System.out.println("       1 2 3");
+		System.out.println("     1 2 3 4 5");
+		System.out.println("   1 2 3 4 5 6 7");
+	}
 }
