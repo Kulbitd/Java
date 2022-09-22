36243bef130875157950c13425c74def97ca5a2b
Данил <kulbit@MacBook-Pro-Danil.local>
2022-09-22 19:59:53 +0300

    Определил метод equals

--- KulbakaMainPz/src/pz2.java
+++ KulbakaMainPz/src/pz2.java
@@ -2,8 +2,13 @@
 public class pz2 {
 	public static void main(String[] args) {
         
-        Points One = new Points();
-        System.out.print( One.toString() );
+        Points One = new Points(1, 3);
+        System.out.println( One.toString() );
+        
+        Points Two = new Points(1, 3);
+        System.out.println( Two.toString() );
+        
+        System.out.println( One.equals(Two) );
     }
 static class Points{
 	double coordinate_x;
@@ -19,6 +24,13 @@ static class Points{
 	public String toString() {
 		String string = "X : " + coordinate_x + " ; Y : " + coordinate_y;
 		return string;
+	}
+	
+	public boolean equals(Points a) {
+		if (coordinate_x == a.coordinate_x && coordinate_y == a.coordinate_y) {
+			return true;
+		}
+		return false;
 	}
 	
 	
