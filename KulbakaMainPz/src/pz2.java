e433561febff9eb9ef8da9d5b6310db711f12fa1
Данил <kulbit@MacBook-Pro-Danil.local>
2022-09-22 19:53:05 +0300

    Определил метод toString

--- KulbakaMainPz/src/pz2.java
+++ KulbakaMainPz/src/pz2.java
@@ -1,4 +1,27 @@
 
 public class pz2 {
-
+	public static void main(String[] args) {
+        
+        Points One = new Points();
+        System.out.print( One.toString() );
+    }
+static class Points{
+	double coordinate_x;
+	double coordinate_y;
+	Points(){
+		coordinate_x = Math.random() * 100;
+		coordinate_y = Math.random() * 100;
+	}
+	Points(int a,int b){
+		coordinate_x = a;
+		coordinate_y = b;
+	}
+	public String toString() {
+		String string = "X : " + coordinate_x + " ; Y : " + coordinate_y;
+		return string;
+	}
+	
+	
+	
+}
 }
